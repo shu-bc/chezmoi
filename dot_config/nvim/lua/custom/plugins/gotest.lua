54e301609a03ebf6c879df092fae37b12fa11078
Shu <shu@baseconnect.in>
2023-03-07 23:02:29 +0900

    improve gotest

--- dot_config/nvim/lua/custom/plugins/gotest.lua
+++ dot_config/nvim/lua/custom/plugins/gotest.lua
@@ -4,7 +4,7 @@ local gotest = function (opt)
 
   local parent_dir = vim.fn.expand('%:p:h')
   local path = vim.fn.substitute(parent_dir, vim.fn.getcwd(), '.', '')
-  local cmd = {"godotenv", "go", "test", path .. '/...' }
+  local cmd = {"godotenv", "go", "test", path .. '/...', '-json'}
 
   if target ~= "" then
     table.insert(cmd, "-run")
@@ -16,23 +16,59 @@ local gotest = function (opt)
   local buf = vim.api.nvim_create_buf(true, true)
   vim.api.nvim_win_set_buf(win, buf)
 
-  vim.fn.jobstart({"go", "clean", "-testcache"})
+  local state = {
+    bufnr = buf,
+    tests = {},
+  }
+
   vim.fn.jobstart(cmd, {
     stdout_buffered = true,
+
     on_stdout = function (_, data)
-      if data then
-        vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
+      if not data then
+        return
+      end
+
+      for _, line in ipairs(data) do
+        local decoded = vim.json.decode(line)
+
+        if decoded.Action == "run" then
+          state.tests[decoded.Test] = {
+            name = decoded.Test,
+            result = "",
+            output = {},
+          }
+        elseif decoded.Action == "output" then
+          if not decoded.Test then
+            return
+          end
+
+          table.insert(state.tests[decoded.Test].output, vim.trim(decoded.Output))
+        elseif decoded.Action == "fail" then
+          state.tests[decoded.Test].result = "fail"
+        end
       end
     end,
+
     on_stderr = function (_, data)
       if data then
-        vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
+        return
       end
+
+      vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
     end,
+
     on_exit = function ()
+      for _, test in pairs(state.tests) do
+        if test.result == "fail" then
+          vim.api.nvim_buf_set_lines(buf, -1, -1, false, {test.name})
+          vim.api.nvim_buf_set_lines(buf, -1, -1, false, test.output)
+        end
+      end
+
       local total_lines = vim.api.nvim_buf_line_count(0)
       vim.api.nvim_win_set_cursor(win, {total_lines, 0})
-    end
+    end,
   })
 end
 
